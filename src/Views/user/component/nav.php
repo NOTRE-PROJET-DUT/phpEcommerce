d39449aeaeb61a10534434b838936addf53b8ff1
NabilBoussalham <nabilboussalham96@gmail.com>
2023-12-01 12:07:50 +0100

    update

--- src/Views/user/component/nav.php
+++ src/Views/user/component/nav.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg blur border-radius-sm top-0 z-index-3 shadow position-absolute my-3 py-2 start-0 end-0 mx-4">
     <div class="container-fluid px-1">
       <a class="navbar-brand font-weight-bolder ms-lg-0 " href="../pages/homePage.php">
-        Corporate UI
+      Nabil-Bilal
       </a>
       <button class="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse" data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon mt-2">
