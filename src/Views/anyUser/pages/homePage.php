1be25c756463e05d4bae9923267899a3e34f8547
BILALMRN <bilalmrn.mail@gmail.com>
2023-12-06 18:49:39 +0100

    chore : customize page product

--- src/Views/anyUser/pages/homePage.php
+++ src/Views/anyUser/pages/homePage.php
@@ -35,7 +35,7 @@
                 $products = $listProduct->getProducts();
                 foreach ($products as $product) : ?>
                     <div class="col mb-5">
-                        <a href="##">
+                        <a  href="product.php?idProduct=<?php echo $product['product_id']; ?>">
                             <div class="card h-100">
                                 <!-- Product image-->
                                 <img class="card-img-top" src="<?php echo $product['image_url']; ?>" alt="..." />
@@ -43,7 +43,7 @@
                                 <div class="card-body p-4">
                                     <div class="text-center">
                                         <!-- Product name-->
-                                        <h5 class="fw-bolder"><?php echo $product['product_name']; ?></h5>
+                                        <h5 class="fw-bolder" ><?php echo $product['product_name']; ?></h5>
                                         <!-- Product reviews-->
                                         <!-- <div class="d-flex justify-content-center small text-warning mb-2">
                                             <div class="bi-star-fill"></div>
@@ -53,12 +53,12 @@
                                             <div class="bi-star-fill"></div>
                                         </div> -->
                                         <!-- Product price-->
-                                        <?php echo $product['price'] . " $"; ?>
+                                        <h5><?php echo $product['price'] . " $"; ?></h5>
                                     </div>
                                 </div>
                                 <!-- Product actions-->
                                 <div class="card-footer p-4 pt-0 border-top-0 bg-transparent" onclick="addToCart(<?php echo $product['product_id'].',\''.$product['product_name'].'\','.$product['price'].',\''.$product['image_url'].'\''; ?>)">
-                                    <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Add to cart</a></div>
+                                    <div class="text-center"><a class="btn btn-outline-dark mt-auto">Add to cart</a></div>
                                 </div>
                             </div>
                         </a>
--- src/Views/anyUser/pages/product.php
+++ src/Views/anyUser/pages/product.php
@@ -1,3 +1,10 @@
+<?php
+$idProduct = 1;
+if (isset($_GET['idProduct'])) {
+    // Get the value of the 'data' parameter
+    $idProduct = $_GET['idProduct'];
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -18,10 +25,10 @@
             <?php
             include '../../../Models/product.php';
             $listProduct = new Product();
-            $product = $listProduct->getProduct(1);
+            $product = $listProduct->getProduct($idProduct);
             ?>
             <div class="row gx-4 gx-lg-5 align-items-center">
-                <div class="col-md-6"><img class="card-img-top mb-5 mb-md-0" src="https://dummyimage.com/600x700/dee2e6/6c757d.jpg" alt="..." /></div>
+                <div class="col-md-6"><img class="card-img-top mb-5 mb-md-0" src="<?php echo $product['image_url']; ?>" alt="...imageProduct" /></div>
                 <div class="col-md-6">
                     <div class="small mb-1"><?php echo $product['category']; ?></div>
                     <h1 class="display-5 fw-bolder"><?php echo $product['product_name']; ?></h1>
@@ -32,7 +39,7 @@
                     <p class="lead"><?php echo $product['description']; ?></p>
                     <div class="d-flex">
                         <input class="form-control text-center me-3" id="inputQuantity" type="num" value="1" style="max-width: 3rem" />
-                        <button class="btn btn-outline-dark flex-shrink-0" type="button">
+                        <button class="btn btn-outline-dark flex-shrink-0" type="button" onclick="addStockToCart(<?php echo $product['product_id'] . ',\'' . $product['product_name'] . '\',' . $product['price'] . ',\'' . $product['image_url'] . '\''; ?>)">
                             <i class="bi-cart-fill me-1"></i>
                             Add to cart
                         </button>
@@ -50,35 +57,24 @@
                 // include '../../../Models/product.php';
                 // $listProduct = new Product();
                 $products = $listProduct->getProductsByCategory($product['category']);
-                foreach ($products as $p) : ?>
+                foreach ($products as $product) : ?>
                     <div class="col mb-5">
-                        <a href="##">
+                        <a href="product.php?idProduct=<?php echo $product['product_id']; ?>">
                             <div class="card h-100">
                                 <!-- Product image-->
-                                <img class="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+                                <img class="card-img-top" src="<?php echo $product['image_url']; ?>" alt="..." />
                                 <!-- Product details-->
                                 <div class="card-body p-4">
                                     <div class="text-center">
                                         <!-- Product name-->
-                                        <h5 class="fw-bolder"><?php echo $p['product_name']; ?></h5>
-                                        <!-- Product reviews-->
-                                        <div class="d-flex justify-content-center small text-warning mb-2">
-                                            <div class="bi-star-fill"></div>
-                                            <div class="bi-star-fill"></div>
-                                            <div class="bi-star-fill"></div>
-                                            <div class="bi-star-fill"></div>
-                                            <div class="bi-star-fill"></div>
-                                        </div>
+                                        <h5 class="fw-bolder"><?php echo $product['product_name']; ?></h5>
                                         <!-- Product price-->
-                                        <div class="fs-5 mb-1">
-                                            <span class="text-decoration-line-through">$45.00</span>
-                                            <span><?php echo $p['price'] . " $"; ?></span>
-                                        </div>
+                                        <h5><?php echo $product['price'] . " $"; ?></h5>
                                     </div>
                                 </div>
                                 <!-- Product actions-->
-                                <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                                    <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="#">Add to cart</a></div>
+                                <div class="card-footer p-4 pt-0 border-top-0 bg-transparent" onclick="addToCart(<?php echo $product['product_id'] . ',\'' . $product['product_name'] . '\',' . $product['price'] . ',\'' . $product['image_url'] . '\''; ?>)">
+                                    <div class="text-center"><a class="btn btn-outline-dark mt-auto">Add to cart</a></div>
                                 </div>
                             </div>
                         </a>
@@ -93,7 +89,57 @@
             <p class="m-0 text-center text-white">Copyright &copy; Your Website 2023</p>
         </div>
     </footer>
+    <script>
+        function addToCart(product_id, name, price, image_url) {
+            // Retrieve existing cart data from localStorage
+            var cart = JSON.parse(localStorage.getItem('cart')) || [];
 
+            // Check if the product is already in the cart
+            var productInCart = cart.find(item => item.id === product_id);
+
+            if (productInCart) {
+                // If the product is in the cart, increase the quantity
+                productInCart.quantity++;
+            } else {
+                // If the product is not in the cart, add it
+                cart.push({
+                    id: product_id,
+                    name: name,
+                    price: price,
+                    srcUrl: image_url,
+                    quantity: 1
+                });
+            }
+
+            // Save the updated cart data back to localStorage
+            localStorage.setItem('cart', JSON.stringify(cart));
+        }
+
+        function addStockToCart(product_id, name, price, image_url) {
+            // Retrieve existing cart data from localStorage
+            var cart = JSON.parse(localStorage.getItem('cart')) || [];
+
+            // Check if the product is already in the cart
+            var productInCart = cart.find(item => item.id === product_id);
+            let quantity = document.getElementById("inputQuantity").value;
+            if (productInCart) {
+                // If the product is in the cart, increase the quantity
+                productInCart.quantity = quantity;
+            } else {
+                // If the product is not in the cart, add it
+                cart.push({
+                    id: product_id,
+                    name: name,
+                    price: price,
+                    srcUrl: image_url,
+                    quantity: quantity
+                });
+            }
+
+            // Save the updated cart data back to localStorage
+            localStorage.setItem('cart', JSON.stringify(cart));
+        }
+    </script>
 </body>
 
 </html>
