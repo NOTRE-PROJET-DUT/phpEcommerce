1cd07ca790f6c00295349f2fbf6ef28fc3be4c6b
BILALMRN <bilalmrn.mail@gmail.com>
2023-12-09 11:18:55 +0100

    build : add script to run the app

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+// Change directory to user views and start PHP server on localhost:8000 in the background
+$userCommand = 'cd src/Views/user; php -S localhost:8000 > index.log 2>&1 &';
+shell_exec($userCommand);
+
+// Change directory to admin views and start PHP server on localhost:8001 in the background
+$adminCommand = 'cd src/Views/admin; php -S localhost:8001 > index.log 2>&1 &';
+shell_exec($adminCommand);
+
+?>
+
