221b4a7b698d2df65e29264a2dff4cf354d06406
BILALMRN <bilalmrn.mail@gmail.com>
2023-12-14 13:23:22 +0100

    fix : change name

--- src/Views/user/pages/forgetPassword.php
+++ src/Views/user/pages/forgetPassword.php
@@ -17,7 +17,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             session_start();
         }
         $_SESSION['emailExist'] = $email;
-          header('Location: ./changepassword');
+          header('Location: ./changePassword');
         } else {
           echo "no exist";
         }
